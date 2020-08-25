c4ebfe1943b10cb359dfe0c7a2394bfd5cd796c9
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-25 14:21:46 +0200

    Added header for Sidebar component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
 import './Sidebar.css'
 
 const Sidebar = () => {
     return (
-        <div>
-            
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <div className="sidebar__info">
+                    <h2>Users</h2>
+                    <h3>
+                        <FiberManualRecordIcon />
+                        Nazwa
+                    </h3>
+                </div>
+                <CreateIcon />
+            </div>
         </div>
     )
 }
