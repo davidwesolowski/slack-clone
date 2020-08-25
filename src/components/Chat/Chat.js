e6aefea6bb3e756da7fa5742c3476ad151c74606
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-25 16:55:51 +0200

    Added header in Chat component with channel name

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import { useParams } from 'react-router-dom';
+import db from '../../firebase';
 import './Chat.css';
 
 const Chat = () => {
+
+    const { id } = useParams();
+    const [roomDetails, setRoomDetails] = useState(null);
+
+    useEffect(() => {
+        db.collection('rooms').doc(id).onSnapshot(snapshot => setRoomDetails(snapshot.data()));
+    }, [id]);
+
     return (
-        <div>
-            
+        <div className="chat">
+            <div className="chat__header">
+                <div className="chat__headerLeft">
+                    <h4 className="chat__channelName">
+                        <strong># {roomDetails?.name}</strong>
+                        <StarBorderOutlinedIcon />
+                    </h4>
+                </div>
+                <div className="chat__headerRight">
+                    <p>
+                        <InfoOutlinedIcon /> Details
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
