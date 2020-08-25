5ca3f06fa2eaaf7fbe4b37b82a533a40f321f5b9
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-25 15:26:52 +0200

    Added functionality to add channels

--- src/components/SidebarOption/SidebarOption.js
+++ src/components/SidebarOption/SidebarOption.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import db from '../../firebase';
 import './SidebarOption.css';
 
 const SidebarOption = ({ Icon, title, id, addChannelOption }) => {
+
+    const addChannel = () => {
+        const channelName = prompt('Eneter your channel name:');
+        if (channelName) {
+            db.collection('rooms').add({
+                name: channelName
+            });
+        }
+    };
+
     return (
         <div className="sidebarOption">
             {
