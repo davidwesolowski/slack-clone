9ed004c241521ce3bc3ee15dab47f61ee131743b
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-25 14:50:50 +0200

    Added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAjn0fKowC9UfSn3HuskyDZSRBBI3CJYBo",
+    authDomain: "slack-app-f76f9.firebaseapp.com",
+    databaseURL: "https://slack-app-f76f9.firebaseio.com",
+    projectId: "slack-app-f76f9",
+    storageBucket: "slack-app-f76f9.appspot.com",
+    messagingSenderId: "248248415784",
+    appId: "1:248248415784:web:e98802ec896d2e0f53271d"
+  };
+
