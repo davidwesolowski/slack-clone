ce4d5ee3bc1aa91356faccd95f0e93da5138ff00
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-25 14:43:38 +0200

    Added sidebar options to sidebar component

--- src/components/SidebarOption/SidebarOption.js
+++ src/components/SidebarOption/SidebarOption.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './SidebarOption.css';
 
-const SidebarOption = () => {
+const SidebarOption = ({ Icon, title }) => {
     return (
         <div className="sidebarOption">
-            <h3 className="sidebarOption__channel">
-                <span className="sidebarOption__hash">#</span>Title
-            </h3>
+            {
+                Icon && <Icon className="sidebarOption__icon"/>
+            }
+            {
+                Icon ? 
+                    (<h3>{title}</h3>) 
+                : 
+                <h3 className="sidebarOption__channel">
+                    <span className="sidebarOption__hash">#</span>{title}
+                </h3>
+            } 
         </div>
     )
 }
