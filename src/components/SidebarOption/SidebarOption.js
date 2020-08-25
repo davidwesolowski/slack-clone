3a8fff7817b06deacc64be14555efe9893f14111
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-25 14:26:26 +0200

    Created basic SidebarOption and its styles

---
+++ src/components/SidebarOption/SidebarOption.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SidebarOption.css';
+
+const SidebarOption = () => {
+    return (
+        <div className="sidebarOption">
+            <h3 className="sidebarOption__channel">
+                <span className="sidebarOption__hash">#</span>Title
+            </h3>
+        </div>
+    )
+}
+
+export default SidebarOption
